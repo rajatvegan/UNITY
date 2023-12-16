266074a311ca3695a8364295661f7c9eb7f01f5b
rajatvegan <rajatkatiyar4@gmail.com>
2023-12-16 11:27:26 +0530

    wsgi to nginx configured

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
 from app import app
 
 if __name__ == "__main__":
-    app.run()
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=8000)
+
 
 # python -m waitress --host=0.0.0.0 --port=8000 wsgi:app
