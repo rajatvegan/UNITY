64a2ea5e5cf4dd49c1e8d664d6c9b3b8f5f943cc
rajatvegan <rajatkatiyar4@gmail.com>
2024-04-06 16:45:10 +0530

    Update Flask route from "/login" to "/signup" in app.py Update port mapping in docker-compose.yml from "80:80" to "80:800" Update port listening in nginx.conf from "listen 80" to "listen 800" Add flask-mail to requirements.txt Update button class in home.html from "login-button" to "signup-button" Delete login.html template file

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__,static_url_path='/static')
 def home():
     return render_template('home.html')
 
-@app.route("/login")
-def login():
-    return render_template('login.html')
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
 
 @app.route("/form")
 def form():
