980a51aaa0d06680c91f4e427ffc62ebe12041fa
rajatvegan <rajatkatiyar4@gmail.com>
2024-06-06 04:18:14 +0530

    a

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,6 +5,3 @@ cd /home/ubuntu/UNITY
 
 # Activate the virtual environment
 source venv/bin/activate
-
-# Ensure all dependencies are installed (optional, since they should already be there)
-pip install -r requirements.txt
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,9 +2,3 @@
 sudo apt-get update -y
 
 sudo apt install python3.12-venv -y
-
-# Navigate to the application directory
-cd /home/ubuntu/UNITY
-
-source venv/bin/activate
-
