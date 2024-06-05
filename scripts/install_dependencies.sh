2323402db8dbe7215539eed42a7c61de875a5315
rajatvegan <rajatkatiyar4@gmail.com>
2024-06-06 02:20:01 +0530

    aaa

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,12 +1,7 @@
-#!/bin/bash
-
 # Update package lists
 sudo apt-get update -y
 
 # Navigate to the application directory
 cd /home/ubuntu/UNITY
 
-sudo apt install -y python3-pip
-# Install Python dependencies
-pip3 install -r requirements.txt
 
