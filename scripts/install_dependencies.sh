13ac8467f8e3dcce898cfba9a971c427408f8506
rajatvegan <rajatkatiyar4@gmail.com>
2024-06-06 01:42:20 +0530

    a

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,6 +6,8 @@ sudo apt-get update -y
 # Install Python and pip if not already installed
 sudo apt-get install -y python3 python3-pip
 
+mkdir UNITY
+
 # Navigate to the application directory
 cd /home/ubuntu/UNITY
 
